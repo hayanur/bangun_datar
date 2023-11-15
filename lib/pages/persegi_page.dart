5c476a93849055cad467d273f3071735a5fd8579
hayanur <lamochi1031@gmail.com>
2023-11-15 13:18:42 +0700

    hitung persegi

---
+++ lib/pages/persegi_page.dart
@@ -0,0 +1,113 @@
+import 'package:bangun_datar_appp/controller/persegi_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PersegiPage extends StatelessWidget {
+  PersegiPage({super.key});
+
+  final PersegiController _persegiController = Get.put(PersegiController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+        title: Text(
+          "Persegi Page",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+            "assets/persegi.png",
+            height: 150,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: Text("Persegi"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+                "Persegi merupakan turunan dari segi empat yang mempunyai ciri khusus keempat sisinya sama panjang dan keempat sudutnya siku-siku (90°)."),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              onChanged: (value) {
+                //akan dieksekusi ketika kita mengetik
+                _persegiController.sisi = int.parse(value);
+              },
+              decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  labelText: "Sisi",
+                  hintText: "Masukkan Sisi",
+                  hintStyle: TextStyle(color: Colors.grey.shade400),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 1, color: Colors.blue),
+                      borderRadius: BorderRadius.all(Radius.circular(10)))),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      _persegiController.hitungLuas();
+                    },
+                  child: Text("Hitung Luas",
+                  style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      padding:  EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      textStyle: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _persegiController.hitungKeliling();
+                  },
+                  child: Text("Hitung Keliling",
+                  style: TextStyle(color: Colors.black54),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.amberAccent,
+                      padding:  EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      textStyle: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Obx(() => Text(_persegiController.hasil.value,
+                style: TextStyle(
+                    color: _persegiController.isHitungLuas.value
+                    ? Colors.green
+                    : Colors.amberAccent, fontWeight: FontWeight.bold),)),
+          )
+        ],
+      ),
+    );
+  }
+}
