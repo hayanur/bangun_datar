701dc46d9694d1af4b483f2a5d6e0a358fcf9303
hayanur <lamochi1031@gmail.com>
2023-11-15 13:18:16 +0700

    hitung persegi

--- lib/controller/persegi_controller.dart
+++ lib/controller/persegi_controller.dart
@@ -1,15 +1,18 @@
 import 'package:get/get.dart';
 
 class PersegiController extends GetxController{
+  RxBool isHitungLuas = true.obs;
   int sisi = 0;
   final hasil = "".obs;
 
   void hitungLuas(){
     int hitung = sisi * sisi;
     hasil.value = "Hasil perhitungan luasnya adalah $sisi x $sisi = $hitung";
+    isHitungLuas.value = true;
   }
   void hitungKeliling(){
     int hitung = 4 * sisi;
     hasil.value = "Hasil perhitungan kelilingnya adalah $sisi x 4 = $hitung";
+    isHitungLuas.value = false;
   }
 }
